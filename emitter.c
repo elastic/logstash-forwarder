5c156cd259beea8c13942dd8c371589f4fd4f603
Jordan Sissel <jls@semicomplete.com>
2012-08-02 18:22:20 -0400

    set hwm

--- emitter.c
+++ emitter.c
@@ -1,3 +1,4 @@
+#define _BSD_SOURCE
 #include "emitter.h"
 #include <zmq.h>
 #include "insist.h"
@@ -10,13 +11,16 @@ void *emitter(void *arg) {
 
   void *socket = zmq_socket(config->zmq, ZMQ_PULL);
   insist(socket != NULL, "zmq_socket() failed: %s", strerror(errno));
+  int64_t hwm = 1;
+  zmq_setsockopt(socket, ZMQ_HWM, &hwm, sizeof(hwm));
   rc = zmq_bind(socket, config->zmq_endpoint);
   insist(rc != -1, "zmq_bind(%s) failed: %s", config->zmq_endpoint,
          zmq_strerror(errno));
 
   for (;;) {
     zmq_msg_t message;
-    zmq_msg_init(&message);
+    rc = zmq_msg_init(&message);
+    insist(rc == 0, "zmq_msg_init failed");
     rc = zmq_recv(socket, &message, 0);
     insist(rc == 0, "zmq_recv(%s) failed (returned %d): %s",
            config->zmq_endpoint, rc, zmq_strerror(errno));
