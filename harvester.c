f829976de66a8857d6ff846fd62d8394611b25e3
Jordan Sissel <jls@semicomplete.com>
2012-08-03 22:24:50 -0700

    Move towards the lumberjack serialized event

--- harvester.c
+++ harvester.c
@@ -6,14 +6,17 @@
 #include <arpa/inet.h> /* for ntohl */
 #include <stdio.h> /* printf and friends */
 #include <zmq.h> /* zeromq messaging library */
-#include <msgpack.h> /* msgpack serialization library */
+#include "str.h" /* dynamic string library */
+#include "proto.h" /* lumberjack wire format serialization */
 
 #include "harvester.h"
 #include "backoff.h"
 #include "insist.h"
 
 #ifdef __MACH__
-/* OS X is dumb, or I am dumb, or we are both dumb. I don't know anymore. */
+/* OS X is dumb, or I am dumb, or we are both dumb. I don't know anymore,
+ * but I need to declare these explicitly even though they are defined
+ * in string.h, unistd.h respectively */
 extern char *strsep(char **stringp, const char *delim);
 extern int gethostname(char *name, size_t namelen);
 #endif
@@ -25,9 +28,14 @@ static struct timespec min_sleep = { 0, 10000000 }; /* 10ms */
 static struct timespec max_sleep = { 15, 0 }; /* 15 */
 
 /* A free function that simply calls free(3) for zmq_msg */
-static inline void free2(void *data, void __attribute__((__unused__)) *hint) {
-  free(data);
-} /* free2 */
+//static inline void free2(void *data, void __attribute__((__unused__)) *hint) {
+  //free(data);
+//} /* free2 */
+
+/* A free function for zmq_msg's with 'struct str' objects */
+static inline void my_str_free(void __attribute__((__unused__)) *data, void *hint) {
+  str_free((struct str *)hint);
+} /* my_str_free */
 
 void *harvest(void *arg) {
   struct harvest_config *config = arg;
@@ -41,8 +49,15 @@ void *harvest(void *arg) {
   hostname_len = strlen(hostname);
 
   fd = open(config->path, O_RDONLY);
-  path_len = strlen(config->path);
   insist(fd >= 0, "open(%s) failed: %s", config->path, strerror(errno));
+
+  path_len = strlen(config->path);
+
+  struct kv event[] = {
+    { "file", 4, config->path, path_len },
+    { "host", 4, hostname, hostname_len },
+    { "line", 4, NULL, 0 }, /* will fill this in later */
+  };
 
   char *buf;
   ssize_t bytes;
@@ -95,25 +110,19 @@ void *harvest(void *arg) {
         } else {
           /* emit line as an event */
           size_t line_len = septok - start;
+          struct str *serialized;
 
-          msgpack_sbuffer *sbuffer = msgpack_sbuffer_new();
-          msgpack_packer *packer = msgpack_packer_new(sbuffer, msgpack_sbuffer_write);
+          /* Set the line */
+          event[2].value = line;
+          event[2].value_len = line_len;
 
-          msgpack_pack_int(packer, 1); /* version */
-
-          msgpack_pack_map(packer, 3); /* host, file, message */
-          msgpack_pack_raw(packer, hostname_len);
-          msgpack_pack_raw_body(packer, hostname, hostname_len);
-          msgpack_pack_raw(packer, path_len);
-          msgpack_pack_raw_body(packer, config->path, path_len);
-          msgpack_pack_raw(packer, line_len);
-          msgpack_pack_raw_body(packer, line, line_len);
+          /* pack using lumberjack data payload */
+          serialized = lumberjack_kv_pack(event, 3 /* 3 elements */);
 
           zmq_msg_t event;
-          zmq_msg_init_data(&event, sbuffer->data, sbuffer->size, NULL, NULL);
+          zmq_msg_init_data(&event, str_data(serialized), str_length(serialized), my_str_free, serialized);
           rc = zmq_send(socket, &event, 0);
           insist(rc == 0, "zmq_send(event) failed: %s", zmq_strerror(rc));
-
           zmq_msg_close(&event);
         }
       } /* for each token */
