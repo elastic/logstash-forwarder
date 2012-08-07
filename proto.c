a0636a27be3d0c7da929ccb0bdef7cc5d07d28a4
Philip Reynolds <philip.reynolds@workday.com>
2012-08-07 05:07:34 +0100

    Corrected %ld to %zd for platform independent format specifiers. Inserted explicit cast for compress2() in zlib.

--- proto.c
+++ proto.c
@@ -308,9 +308,9 @@ int lumberjack_flush(struct lumberjack *lumberjack) {
 
   size_t compressed_length = lumberjack->compression_buffer->data_size;
   /* compress2 is provided by zlib */
-  rc = compress2((Bytef *)str_data(lumberjack->compression_buffer), &compressed_length,
+  rc = compress2((Bytef *)str_data(lumberjack->compression_buffer), (unsigned long *)&compressed_length,
                  (Bytef *)str_data(lumberjack->io_buffer), length, 1);
-  insist(rc == Z_OK, "compress2(..., %ld, ..., %ld) failed; returned %d",
+  insist(rc == Z_OK, "compress2(..., %zd, ..., %zd) failed; returned %d",
          compressed_length, length, rc);
 
   str_truncate(lumberjack->io_buffer);
@@ -397,7 +397,7 @@ static int lumberjack_read_ack(struct lumberjack *lumberjack, uint32_t *sequence
     bytes = SSL_read(lumberjack->ssl, buf + offset, remaining);
     if (bytes <= 0) {
       /* eof(0) or error(<0). */
-      printf("bytes <= 0: %ld\n", (long int) bytes);
+      printf("bytes <= 0: %zd\n", bytes);
       errno = EPIPE; /* close enough? */
       return -1;
     }
