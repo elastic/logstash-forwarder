d2884ccd8c1ee97c8a010a28590e02daee2ccb2d
Jordan Sissel <jls@semicomplete.com>
2012-08-07 04:19:47 -0400

    Support 'window size' frame

--- lumberjack.rb
+++ lumberjack.rb
@@ -4,40 +4,49 @@ require "socket"
 require "thread"
 require "openssl"
 
-Thread.abort_on_exception = true
+#Thread.abort_on_exception = true
 
 def handle(fd)
+  puts fd
   last_ack = 0
-  window_size = 64
+  window_size = 0
+
   while true
-    version = fd.sysread(1)
+    version = fd.read(1)
     #puts "version: #{version}"
-    frame = fd.sysread(1)
+    frame = fd.read(1)
     #puts "frame: #{frame}"
 
-    if frame != "D"
+    if frame == "W" # window size
+      window_size = fd.read(4).unpack("N").first
+      puts "Window size: #{window_size}"
+      next
+    elsif frame != "D"
       puts "Unexpected frame type: #{frame}"
       fd.close
       return
     end
 
     # data frame
-    sequence = fd.sysread(4).unpack("N").first
+    sequence = fd.read(4).unpack("N").first
     #puts "seq: #{sequence}"
-    count = fd.sysread(4).unpack("N").first
+    count = fd.read(4).unpack("N").first
     #puts "count: #{count}"
 
     map = {}
     count.times do 
-      key_len = fd.sysread(4).unpack("N").first
-      key = fd.sysread(key_len);
-      value_len = fd.sysread(4).unpack("N").first
-      value = fd.sysread(value_len);
+      key_len = fd.read(4).unpack("N").first
+      key = fd.read(key_len);
+      value_len = fd.read(4).unpack("N").first
+      value = fd.read(value_len);
       map[key] = value
     end
     #p sequence => map
 
+    #puts "v: #{sequence - last_ack} vs #{window_size}"
     if sequence - last_ack >= window_size
+      #fd.close; return;
+
       # ack this.
       fd.syswrite(["1", "A", sequence].pack("AAN"))
       last_ack = sequence
@@ -51,7 +60,18 @@ sslContext.cert = OpenSSL::X509::Certificate.new(File.read("/tmp/server.crt"))
 sslContext.key = OpenSSL::PKey::RSA.new(File.read("/tmp/server.key"), "asdf")
 sslServer = OpenSSL::SSL::SSLServer.new(server, sslContext)
 
+puts "OK"
 while true
-  Thread.new(sslServer.accept) { |fd| handle(fd) }
+  begin 
+    Thread.new(sslServer.accept) do |fd| 
+      begin
+        handle(fd) 
+      rescue => e
+        p e
+        raise e
+      end
+    end
+  rescue => e
+    p e
+  end
 end
-
