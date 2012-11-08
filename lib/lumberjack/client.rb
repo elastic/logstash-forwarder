d3bb6246c2834396af194ebe1c902daa048a4520
Nick Ethier <ncethier@gmail.com>
2012-11-08 10:30:36 -0700

    add client interface

--- lib/lumberjack/client.rb
+++ lib/lumberjack/client.rb
@@ -9,7 +9,40 @@ module Lumberjack
   SEQUENCE_MAX = (2**(0.size * 8 -2) -1)
 
   class Client
+    def initialize(opts={})
+      @opts = {
+        :port => 0,
+        :addresses => [],
+        :ssl_certificate => nil,
+        :window_size => 5000
+      }.merge(opts)
 
+      @opts[:addresses] = [@opts[:addresses]] if @opts[:addresses].class == String
+      raise "Must set a port." if @opts[:port] == 0
+      raise "Must set atleast one address" if @opts[:addresses].empty? == 0
+      raise "Must set a ssl certificate or path" if @opts[:ssl_certificate].nil?
+
+      @socket = connect
+
+    end
+
+    private
+    def connect
+      addrs = @opts[:addresses].shuffle
+      begin
+        raise "Could not connect to any hosts" if addrs.empty?
+        opts = @opts
+        opts[:address] = addrs.pop
+        Lumberjack::Socket.new(opts)
+      rescue *[Errno::ECONNREFUSED,SocketError]
+        retry
+      end
+    end
+
+    public
+    def write(hash)
+      @socket.write_hash(hash)
+    end
   end
 
   class Socket
@@ -22,16 +55,19 @@ module Lumberjack
     # * :address - the host/address to bind to
     # * :ssl_certificate - the path to the ssl cert to use
     attr_reader :sequence
+    attr_reader :window_size
 
     def initialize(opts={})
       @sequence = 0
       @last_ack = 0
-      @sent = 0
       @opts = {
         :port => 0,
         :address => "127.0.0.1",
         :ssl_certificate => nil,
+        :window_size => 5000
       }.merge(opts)
+
+      @window_size = @opts[:window_size]
 
       tcp_socket = TCPSocket.new(@opts[:address], @opts[:port])
       openssl_cert = OpenSSL::X509::Certificate.new(File.read(@opts[:ssl_certificate]))
@@ -57,13 +93,13 @@ module Lumberjack
         end
       }
 =end
-      @socket.syswrite(["1", "W", Lumberjack::WINDOW_SIZE].pack("AAN"))
+      @socket.syswrite(["1", "W", @window_size].pack("AAN"))
     end
 
     private 
     def inc
-      @sequence = 0 if @sequence > Lumberjack::SEQUENCE_MAX
-      @sequence = @sequence + 1
+      @sequence = 0 if @sequence+1 > Lumberjack::SEQUENCE_MAX
+      @sequence += 1
     end
 
     private
@@ -75,7 +111,7 @@ module Lumberjack
     public
     def write_hash(hash)
       frame = to_frame(hash, inc)
-      ack if (@sequence - @last_ack) >= Lumberjack::WINDOW_SIZE
+      ack if (@sequence - @last_ack) >= @window_size
       #@semaphore.synchronize do
       #  @send << frame
       #end
@@ -84,12 +120,10 @@ module Lumberjack
 
     private
     def ack
-      #puts "CLIENT: ACKING"
       version = @socket.read(1)
       type = @socket.read(1)
       raise "Whoa we shouldn't get this frame: #{type}" if type != "A"
       @last_ack = @socket.read(4).unpack("N").first
-      #puts "CLIENT: ACKED"
       ack if (@sequence - @last_ack) >= Lumberjack::WINDOW_SIZE
     end
 
