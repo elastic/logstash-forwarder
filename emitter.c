f6e60558a84e7135510eeac073dab9b4b3960f39
Jordan Sissel <jls@semicomplete.com>
2013-02-06 01:19:57 -0800

    use flog

--- emitter.c
+++ emitter.c
@@ -115,13 +115,13 @@ void *emitter(void *arg) {
       clock_gettime(CLOCK_MONOTONIC, &now);
       double s = (start.tv_sec + 0.0) + (start.tv_nsec / 1000000000.0);
       double n = (now.tv_sec + 0.0) + (now.tv_nsec / 1000000000.0);
-      fprintf(stderr, "Rate: %f (bytes: %f)\n", (count + 0.0) / (n - s), (bytes + 0.0) / (n - s));
+      flog(stdout, "Rate: %f (bytes: %f)\n", (count + 0.0) / (n - s), (bytes + 0.0) / (n - s));
       struct rusage rusage;
       rc = getrusage(RUSAGE_SELF, &rusage);
       insist(rc == 0, "getrusage failed: %s\n", strerror(errno));
-      printf("cpu user/system: %d.%06d / %d.%06d\n",
-             (int)rusage.ru_utime.tv_sec, (int)rusage.ru_utime.tv_usec,
-             (int)rusage.ru_stime.tv_sec, (int)rusage.ru_stime.tv_usec);
+      flog(stdout, "cpu user/system: %d.%06d / %d.%06d\n",
+           (int)rusage.ru_utime.tv_sec, (int)rusage.ru_utime.tv_usec,
+           (int)rusage.ru_stime.tv_sec, (int)rusage.ru_stime.tv_usec);
       clock_gettime(CLOCK_MONOTONIC, &start);
       bytes = 0;
       count = 0;
