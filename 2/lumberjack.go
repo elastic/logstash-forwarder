8ffeb3d05402f009e6d9ba3076388c8db935b9ab
Jordan Sissel <jls@semicomplete.com>
2013-02-24 23:01:26 -0800

    - add todos/etc

--- 2/lumberjack.go
+++ 2/lumberjack.go
@@ -15,28 +15,37 @@ func emit(events []*lumberjack.FileEvent) {
 
 func main() {
   // TODO(sissel): support flags for setting... stuff
-  // TODO(sissel): need a HarvestForeman to manage the harvester
-  // TODO(sissel): Need an encryptor
-  // TODO(sissel): Need a compressor
-  event_stream := make(chan *lumberjack.FileEvent, 32)
+  event_chan := make(chan *lumberjack.FileEvent, 32)
+  emitter_chan := make(chan *lumberjack.EventEnvelope, 1)
+
+  // The basic model of execution:
+  // - prospector: finds files in paths/globs to harvest, starts harvesters
+  // - harvester: reads a file, sends events to the spooler
+  // - spooler: buffers events until ready to flush to the emitter
+  // - emitter: writes to the network, notifies registrar
+  // - registrar: records positions of files read
+  // Finally, prospector uses the registrar information, on restart, to
+  // determine where in each file to resume a harvester.
+
+  // TODO(sissel): need a prospector scan for files and launch harvesters
+
+  // Example dummy harvester
   h := lumberjack.Harvester{Path: "/var/log/messages"}
-  go h.Harvest(event_stream)
-  go h.Harvest(event_stream)
+  go h.Harvest(event_chan)
+  go h.Harvest(event_chan)
 
-  //flusher = func(events []interface{}) {
-    //fmt.Printf("Flushing %d events\n", len(events))
-  //}
-  
-  var window_size uint64 = 1024
-  timeout := 1 * time.Second
-  emitter_stream := make(chan *lumberjack.EventEnvelope, 1)
+  var window_size uint64 = 1024 // Make this a flag
+  var idle_timeout time.Duration = 1 * time.Second // Make this a flag
+
   // harvester -> spooler
-  go lumberjack.Spooler(event_stream, emitter_stream, window_size, timeout)
+  go lumberjack.Spooler(event_chan, emitter_chan, window_size, idle_timeout)
 
-  // spooler -> emitter
-  for x := range emitter_stream {
+  // spooler -> publisher
+  for x := range emitter_chan {
     // got a bunch of events, ship them out.
     fmt.Printf("Spooler gave me %d events\n", len(x.Events))
   }
-  //lumberjack.Emitter(
+
+  // emitter should send acknowledgements to the registrar
+  // registrar records last acknowledged positions in all files.
 } /* main */
