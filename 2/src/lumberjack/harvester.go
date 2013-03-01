1d1d4c888571a6c08806f71ec4e93247a92484ad
Jordan Sissel <jls@semicomplete.com>
2013-03-01 13:53:22 -0800

    - Give up after 24 hours on idle files (todo: make this configurable) - fix off-by-one on offset counts - support stdin if the path  is simply "-"

--- 2/src/lumberjack/harvester.go
+++ 2/src/lumberjack/harvester.go
@@ -21,11 +21,16 @@ func (h *Harvester) Harvest(output chan *FileEvent) {
   // TODO(sissel): Emit FileEvent for each line to 'output'
   // TODO(sissel): Handle rotation
   // TODO(sissel): Sleep when there's nothing to do
+  // TODO(sissel): Quit if we think the file is dead (file dev/inode changed, no data in X seconds)
 
   fmt.Printf("Starting harvester: %s\n", h.Path)
 
   file := h.open()
+  defer file.Close()
+  info := file.Stat()
+
   // TODO(sissel): Ask the registrar for the start position?
+  // TODO(sissel): record the current file inode/device/etc
 
   var line uint64 = 0 // Ask registrar about the line number
 
@@ -35,12 +40,28 @@ func (h *Harvester) Harvest(output chan *FileEvent) {
   // TODO(sissel): Make the buffer size tunable at start-time
   reader := bufio.NewReaderSize(file, 16<<10) // 16kb buffer by default
 
+  var read_timeout = 10 * time.Second
+  last_read_time = time.Now()
   for {
-    text, err := h.readline(reader)
+    text, err := h.readline(reader, read_timeout)
 
     if err != nil {
-      return
+      if err == io.EOF {
+        // timed out waiting for data, got eof.
+        // TODO(sissel): Check to see if the file was truncated
+        // TODO(sissel): if last_read_time was more than 24 hours ago
+        if age = time.Since(last_read_time); age > (24 * time.Hour) {
+          // This file is idle for more than 24 hours. Give up and stop harvesting.
+          fmt.Printf("Stopping harvest of %s; last change was %d seconds ago\n", h.Path, age.Seconds())
+          return
+        }
+        continue
+      } else {
+        fmt.Printf("Unexpected state reading from %s; error: %s\n", h.Path, err)
+        return
+      }
     }
+    last_read_time = time.Now()
 
     line++
     event := &FileEvent{
@@ -49,14 +70,20 @@ func (h *Harvester) Harvest(output chan *FileEvent) {
       Line: proto.Uint64(line),
       Text: text,
     }
-    offset += int64(len(*event.Text))
+    offset += int64(len(*event.Text)) + 1  // +1 because of the line terminator
 
-    output <- event
+    output <- event // ship the new event downstream
   } /* forever */
 }
 
 func (h *Harvester) open() *os.File {
   var file *os.File
+
+  // Special handling that "-" means to read from standard input
+  if h.Path == "-" {
+    return os.Stdin
+  } 
+
   for {
     var err error
     file, err = os.Open(h.Path)
@@ -71,34 +98,40 @@ func (h *Harvester) open() *os.File {
   }
 
   // TODO(sissel): In the future, use the registrary to determine where to seek.
+  // TODO(sissel): Only seek if the file is a file, not a pipe or socket.
   file.Seek(0, os.SEEK_END)
 
   return file
 }
 
-func (h *Harvester) readline(reader *bufio.Reader) (*string, error) {
+func (h *Harvester) readline(reader *bufio.Reader, eof_timeout time.Duration) (*string, error) {
   var buffer bytes.Buffer
+  start_time = time.Now()
   for {
     segment, is_partial, err := reader.ReadLine()
+
     if err != nil {
-      // TODO(sissel): Handle the error, check io.EOF?
       // TODO(sissel): if eof and line_complete is false, don't check rotation unless a very long time has passed
       if err == io.EOF {
-        time.Sleep(1 * time.Second)
+        time.Sleep(1 * time.Second) // TODO(sissel): Implement backoff
+
+        // Give up waiting for data after a certain amount of time.
+        // If we time out, return the error (eof)
+        if time.Since(start_time) > eof_timeout {
+          return nil, err
+        }
         continue
       } else {
         fmt.Println(err)
-        return nil, err // TODO(sissel): don't do this
+        return nil, err // TODO(sissel): don't do this?
       }
-
-      // TODO(sissel): At EOF, check rotation
-      // TODO(sissel): if nothing to do, sleep
     }
 
     // TODO(sissel): if buffer exceeds a certain length, maybe report an error condition? chop it?
     buffer.Write(segment)
 
     if !is_partial {
+      // If we got a full line, return the whole line.
       str := new(string)
       *str = buffer.String()
       return str, nil
