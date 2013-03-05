bb46b39e31334a25657d219e2f4618766f20a070
Jordan Sissel <jls@semicomplete.com>
2013-03-05 00:59:20 -0800

    - no more protobufs

---
+++ 2/src/lumberjack/event.go
@@ -0,0 +1,8 @@
+package lumberjack
+
+type FileEvent struct {
+  Source *string `json:"source,omitempty"`
+  Offset *uint64 `json:"offset,omitempty"`
+  Line *uint64 `json:"line,omitempty"`
+  Text *string `json:"text,omitempty"`
+}
