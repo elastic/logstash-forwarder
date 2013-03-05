9f418e95d6d53cf3e85d894788ed8a86019e8695
Jordan Sissel <jls@semicomplete.com>
2013-03-05 00:59:37 -0800

    - start to make things configurable

--- 2/lumberjack.go
+++ 2/lumberjack.go
@@ -1,23 +1,57 @@
 package main
 
 import (
-  "fmt"
+  "log"
   "lumberjack"
   "os"
   "time"
-  proto "code.google.com/p/goprotobuf/proto"
+  "flag"
+  "strings"
+  "runtime/pprof"
 )
 
-func emit(events []*lumberjack.FileEvent) {
-  envelope := &lumberjack.EventEnvelope{Events: events}
-  data, _ := proto.Marshal(envelope)
-  fmt.Printf("Flushing !!! %d events: %d encoded\n", len(events), len(data))
-} /* emit */
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
+var spool_size = flag.Uint64("spool-size", 1024, "Maximum number of events to spool before a flush is forced.")
+var idle_timeout = flag.Duration("idle-flush-time", 5 * time.Second, "Maximum time to wait for a full spool before flushing anyway")
+var server_timeout = flag.Duration("server-timeout", 30 * time.Second, "Maximum time to wait for a request to a server before giving up and trying another.")
+var servers = flag.String("servers", "", "Server (or comma-separated list of servers) to send events to. Each server can be a 'host' or 'host:port'. If the port is not specified, port 5005 is assumed. One server is chosen of the list at random, and only on failure is another server used.")
 
 func main() {
+  flag.Parse()
+
+  if *cpuprofile != "" {
+    f, err := os.Create(*cpuprofile)
+    if err != nil {
+        log.Fatal(err)
+    }
+    pprof.StartCPUProfile(f)
+    go func() {
+      time.Sleep(60 * time.Second)
+      pprof.StopCPUProfile()
+      panic("done")
+    }()
+  }
+
+  // Turn 'host' and 'host:port' into 'tcp://host:port'
+  if *servers == "" {
+    log.Printf("No servers specified, please provide the -servers setting\n")
+    return
+  }
+  server_list := strings.Split(*servers, ",")
+  for i, server := range server_list {
+    if !strings.Contains(server, ":") {
+      server_list[i] = "tcp://" + server + ":5005"
+    } else {
+      server_list[i] = "tcp://" + server
+    }
+  }
+
+  log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+
   // TODO(sissel): support flags for setting... stuff
-  event_chan := make(chan *lumberjack.FileEvent, 32)
-  publisher_chan := make(chan *lumberjack.EventEnvelope, 1)
+  event_chan := make(chan *lumberjack.FileEvent, 16)
+  publisher_chan := make(chan []*lumberjack.FileEvent, 1)
 
   // The basic model of execution:
   // - prospector: finds files in paths/globs to harvest, starts harvesters
@@ -28,18 +62,13 @@ func main() {
   // Finally, prospector uses the registrar information, on restart, to
   // determine where in each file to resume a harvester.
 
-  // TODO(sissel): need a prospector scan for files and launch harvesters
-
   // Prospect the globs/paths given on the command line and launch harvesters
-  go lumberjack.Prospect(os.Args[1:], event_chan)
-
-  var window_size uint64 = 1024 // Make this a flag
-  var idle_timeout time.Duration = 1 * time.Second // Make this a flag
+  go lumberjack.Prospect(flag.Args(), event_chan)
 
   // Harvesters dump events into the spooler.
-  go lumberjack.Spool(event_chan, publisher_chan, window_size, idle_timeout)
+  go lumberjack.Spool(event_chan, publisher_chan, *spool_size, *idle_timeout)
 
-  lumberjack.Publish(publisher_chan)
+  lumberjack.Publish(publisher_chan, server_list, *server_timeout)
 
   // TODO(sissel): publisher should send state to the registrar
   // TODO(sissel): registrar records last acknowledged positions in all files.
