7c4255e555221c102cb94eb8b0494ade35d3b249
Jordan Sissel <jls@semicomplete.com>
2013-04-05 21:13:11 -0700

    - move files around

---
+++ src/sodium/nonce.go
@@ -0,0 +1,36 @@
+package sodium
+
+func RandomNonceStrategy() (func() [crypto_box_NONCEBYTES]byte) {
+  return func () (nonce [crypto_box_NONCEBYTES]byte) {
+    Randombytes(nonce[:])
+    return
+  }
+}
+
+func IncrementalNonceStrategy() (func() [crypto_box_NONCEBYTES]byte) {
+  var nonce [crypto_box_NONCEBYTES]byte
+  Randombytes(nonce[:])
+
+  // TODO(sissel): Make the high-8 bytes of the nonce based on current time to
+  // help avoid collisions?
+
+  return func() ([crypto_box_NONCEBYTES]byte) {
+    increment(nonce, 1)
+    return nonce
+  }
+}
+
+func increment(bytes []byte, value uint64) {
+  for offset, carry := 0, false; carry == true || offset == 0; offset += 8 { 
+    ptr := (*uint64)(unsafe.Pointer((&bytes[offset])))
+    old := *ptr
+    *ptr += value
+    if old > *ptr {
+      // overflow, carry and continue
+      value = 1
+      carry = true
+    } else {
+      carry = false
+    }
+  }
+}
