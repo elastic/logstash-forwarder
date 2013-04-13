c5f1aad984cbd03c9a8fd63ce1534e09ef2ee81a
Jordan Sissel <jls@semicomplete.com>
2013-04-12 17:44:42 -0700

    - purge C stuff; it's in a separate branch now

--- c/sleepdefs.h
+++
@@ -1,5 +0,0 @@
-#ifndef _SLEEPDEFS_H_
-#define _SLEEPDEFS_H_
-static struct timespec MIN_SLEEP = { 0, 10000000 }; /* 10ms */
-static struct timespec MAX_SLEEP = { 5, 0 };
-#endif /* _SLEEPDEFS_H_ */
