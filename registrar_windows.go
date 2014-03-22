d23bf70b6923a54148c3e82dad2fa83f815dadc6
Driskell <devel@jasonwoods.me.uk>
2014-03-22 13:49:10 +0000

    Report registrar problem if we cannot save the registrar file - and fix broken registrar on Windows due to ".old" file already existing

--- registrar_windows.go
+++ registrar_windows.go
@@ -19,6 +19,25 @@ func WriteRegistry(state map[string]*FileState, path string) {
   file.Close()
 
   old := path + ".old"
-  os.Rename(path, old)
-  os.Rename(tmp, path)
+
+  if _, err = os.Stat(old); err != nil && os.IsNotExist(err) {
+    err = nil
+  } else {
+    err = os.Remove(old)
+  }
+
+  if err != nil {
+    log.Printf("Registrar save problem: Failed to delete backup file: %s\n", err)
+  }
+
+  err = os.Rename(path, old)
+  if err != nil {
+    log.Printf("Registrar save problem: Failed to perform backup: %s\n", err)
+    return
+  }
+
+  err = os.Rename(tmp, path)
+  if err != nil {
+    log.Printf("Registrar save problem: Failed to move the new file into place: %s\n", err)
+  }
 }
