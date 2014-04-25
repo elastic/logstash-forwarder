48ba24ef7b3bc1bbd0b722b0ca3cb8affeb9a029
Driskell <devel@jasonwoods.me.uk>
2014-04-25 15:32:48 +0100

    Enable build on openbsd to fix #194

---
+++ filestate_openbsd.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "os"
+  "syscall"
+)
+
+type FileState struct {
+  Source *string `json:"source,omitempty"`
+  Offset int64   `json:"offset,omitempty"`
+  Inode  uint64  `json:"inode,omitempty"`
+  Device int32  `json:"device,omitempty"`
+}
+
+func file_ids(info *os.FileInfo, state *FileState) {
+  fstat := (*info).Sys().(*syscall.Stat_t)
+  state.Inode = fstat.Ino
+  state.Device = fstat.Dev
+}
+
+func is_filestate_same(path string, info os.FileInfo, state *FileState) bool {
+  istate := &FileState{}
+  file_ids(&info, istate)
+  return (istate.Inode == state.Inode && istate.Device == state.Device)
+}
+
+func open_file_no_lock(path string) (*os.File, error) {
+  return os.Open(path)
+}
