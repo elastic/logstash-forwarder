86535c52fe293fa00a598db71626f25ec8213724
Driskell <devel@jasonwoods.me.uk>
2014-05-01 20:05:00 +0100

    Remove redundant comment

--- registrar.go
+++ registrar.go
@@ -14,9 +14,7 @@ func Registrar(new_state map[string]*FileState, input chan []*FileEvent) {
       if *event.Source == "-" {
         continue
       }
-      // have to dereference the FileInfo here because os.FileInfo is an
-      // interface, not a struct, so Go doesn't have smarts to call the Sys()
-      // method on a pointer to os.FileInfo. :(
+
       ino, dev := file_ids(event.fileinfo)
       state[*event.Source] = &FileState{
         Source: event.Source,
