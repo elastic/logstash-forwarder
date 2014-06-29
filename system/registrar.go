de07fff4748bbfea67e497fb4f9e3376c36d4d12
Joubin Houshyar <joubin.houshyar@elasticsearch.com>
2014-06-29 08:15:19 -0400

    NOP REVU and TODO notes

--- system/registrar.go
+++ system/registrar.go
@@ -1,47 +1,17 @@
 package system
 
-/* System Documents & Records are maintained by the system.Registrar.
- * This file contains the various types and funcs that provide
- * the functionality.
- */
+// registrar.go: implementation of lsf/system.Registrar
+
 import (
 	"fmt"
 )
-
-//// ----------------------------------------------------------------------------
-//// Registrar
-//// ----------------------------------------------------------------------------
-//
-//type Registrar interface {
-//	// Reads the document and returns snapshot value.
-//	// No locks held. No files open
-//	ReadDocument(DocId) (Document, error)
-//	// Creates new document with given map (of record data).
-//	// Returns the document snapshot (per ReadDocument)
-//	// No locks head. No files open
-//	CreateDocument(DocId, map[string][]byte) (Document, error)
-//	// Saves document (if dirty) - dirty flag cleared; otherwise NOP.
-//	// Write Lock acquired for duration (attempted)
-//	// New document file is atomically swapped.
-//	UpdateDocument(Document) (bool, error)
-//	//
-//	DeleteDocument(DocId) (bool, error)
-//	// stop.
-//	// release all resources.
-//	Stop() chan<- struct{}
-//	// signals Registrar stopped.
-//	// signals all resources released.
-//	Done() <-chan stat
-//	// identity info & status
-//	String() string
-//}
 
 func StartRegistry(path string) (Registrar, error) {
 	r, e := openRegistry(path)
 	if e != nil {
 		return nil, e
 	}
-	ui := make(chan req, 12)
+	ui := make(chan req, 12) 			// REVU: magic number.. aim is to allow fast enqueue of requests.
 	cancel := make(chan struct{}, 1)
 	done := make(chan stat, 1)
 
@@ -50,6 +20,29 @@ func StartRegistry(path string) (Registrar, error) {
 
 	return registrar, nil
 }
+
+// Launches a goroutine to process user requests from a request queue.
+// This mechanism provides the in-memory linearlization of access to the
+// shared system resources managed by system, per semantics of system.Registrar
+func beTheRegistrar(r *registry, ui <-chan req, cancel <-chan struct{}, done chan<- stat) {
+	defer func() {
+		// REVU: request.execute() returns errors via channels
+		//       and wraps calls to lsf/lsfun functions (which
+		//       are not to panic(?)). << TODO: affirm
+	}()
+
+	for {
+		select {
+		case request := <-ui:
+			// REVU: TODO:
+			request.result <- request.execute()
+		case <-cancel:
+			done <- stat{nil, NilValue}
+			return
+		}
+	}
+}
+
 
 // ----------------------------------------------------------------------------
 // registrar
@@ -79,7 +72,6 @@ func (r *registrar) Stop() chan<- struct{} { return r.cancel }
 func (r *registrar) DeleteDocument(key DocId) (bool, error) {
 	resch := makeResChan()
 	fn := func() interface{} {
-		//		log.Printf("func: get document %s", string(key))
 		ok, e := r.reg.deleteDocument(key)
 		if e != nil {
 			return e
@@ -94,7 +86,6 @@ func (r *registrar) DeleteDocument(key DocId) (bool, error) {
 func (r *registrar) UpdateDocument(doc Document) (bool, error) {
 	resch := makeResChan()
 	fn := func() interface{} {
-		//		log.Printf("func: update document %s", string(doc.Id()))
 		ok, e := r.reg.updateDocument(doc.(*document))
 		if e != nil {
 			return e
@@ -108,7 +99,6 @@ func (r *registrar) UpdateDocument(doc Document) (bool, error) {
 func (r *registrar) ReadDocument(key DocId) (Document, error) {
 	resch := makeResChan()
 	fn := func() interface{} {
-		//		log.Printf("func: get document %s", string(key))
 		doc, e := r.reg.readDocument(key)
 		if e != nil {
 			return e
@@ -123,7 +113,6 @@ func (r *registrar) ReadDocument(key DocId) (Document, error) {
 func (r *registrar) CreateDocument(key DocId, data map[string][]byte) (Document, error) {
 	resch := makeResChan()
 	fn := func() interface{} {
-		//		log.Println("func: create document: %q", string(key))
 		doc, e := r.reg.createDocument(key, data)
 		if e != nil {
 			return e
@@ -136,9 +125,8 @@ func (r *registrar) CreateDocument(key DocId, data map[string][]byte) (Document,
 }
 
 // ----------------------------------------------------------------------------
-// request dispatch mechanism
+// concurrent async request dispatch mechanism
 // ----------------------------------------------------------------------------
-
 
 type stat struct {
 	err error
@@ -176,22 +164,5 @@ func mapBoolResult(result interface{}) (bool, error) {
 		return false, t
 	default:
 		panic("BUG - unexpected type value")
-	}
-}
-
-func beTheRegistrar(r *registry, ui <-chan req, cancel <-chan struct{}, done chan<- stat) {
-	defer func() {}()
-
-	for {
-		select {
-		case request := <-ui:
-			// process request
-			//			log.Printf("@request: Registrar: process request %s", request)
-			request.result <- request.execute()
-		case <-cancel:
-			//			log.Println("@cancel: Registrar: stopping")
-			done <- stat{nil, NilValue}
-			return
-		}
 	}
 }
