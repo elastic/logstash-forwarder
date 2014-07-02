aabd4d8c52c21a829068f7260e7822ac03aab25c
Joubin Houshyar <joubin.houshyar@elasticsearch.com>
2014-07-02 11:44:00 -0400

    ADD lslib/RotatingFileWriter
    Intended uses are for simulating loggers for tests/dev and
    as Writer for system journal/log files e.g. tracker logs.

--- development/tools/rotating-logger/faux-rotating-logger.go
+++ development/tools/rotating-logger/faux-rotating-logger.go
@@ -4,15 +4,23 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"lsf/lslib"
 	"lsf/panics"
 	"os"
-	"path"
 	"time"
 )
 
+var sequence int64
+
+func simulateLogInput() []byte {
+	line := fmt.Sprintf("%d %019d INFO simulated single line sequenced log entry\n", time.Now().UnixNano(), sequence)
+	sequence++
+	return []byte(line)
+}
+
 var config struct {
 	path, filename string
-	maxsize        uint64
+	maxsize        int64
 	maxfiles       uint
 	delay          time.Duration
 	fileperm       os.FileMode
@@ -26,10 +34,12 @@ func init() {
 	log.SetFlags(0)
 	flag.StringVar(&config.path, "path", ".", "path to log file dir")
 	flag.StringVar(&config.filename, "name", "", "basename for log files")
-	flag.Uint64Var(&config.maxsize, "size", uint64(16777216), "max size of each log file")
+	flag.Int64Var(&config.maxsize, "size", int64(16777216), "max size of each log file")
 	flag.UintVar(&config.maxfiles, "num", uint(16), "max number of rotated filesa")
 	flag.UintVar(&delayOpt, "f", uint(10), "microsec delay between each log event")
 	flag.UintVar(&filemode, "m", uint(0644), "microsec delay between each log event")
+
+	panics.DEBUG = true
 }
 
 // Simulate a rotating log writer.
@@ -45,55 +55,27 @@ func main() {
 		os.Exit(0)
 	}
 	stop, wdone := newProcess()
-	go writeLog(config.path, config.filename, int64(config.maxsize), config.maxfiles, config.delay, stop, wdone)
+	go writeLog(config.path, config.filename, config.maxsize, config.maxfiles, config.delay, stop, wdone)
 
 	log.Println(<-wdone)
 }
 
-func writeLog(dir, basename string, maxsize int64, maxfiles uint, delay_msec time.Duration, stop <-chan interface{}, wdone chan<- interface{}) {
+func writeLog(basepath, basename string, maxFileSize int64, maxfiles uint, delay_msec time.Duration, stop <-chan interface{}, wdone chan<- interface{}) {
 	defer panics.AsyncRecover(wdone, "ok")
 
-	fname := path.Join(dir, basename)
-	file, e := os.OpenFile(fname, os.O_WRONLY|os.O_CREATE, config.fileperm)
-	if e != nil {
-		file, e = os.OpenFile(fname, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, config.fileperm)
-		panics.OnError(e, "os.OpenFile", "CREATE|TRUNC", fname)
-	}
-
-	rotator, e := GetFileRotator(file, dir, 0, config.maxfiles)
-	panics.OnError(e, "GetFileRotator")
-
-	// continuing from an earlier run?
-	// note: won't pick up the sequence so it will overwrite ..
-	//       sipmly will append to main log file
-	n, _ := file.Seek(0, os.SEEK_END)
-	if n > maxsize {
-		file, e = rotator.Next()
-		panics.OnError(e, "rotate", "on-startup")
-		n = 0
-	}
+	rotator, e := lslib.NewRotatingFileWriter(basepath, basename, maxfiles, maxFileSize)
+	panics.OnError(e, "NewFileRotator")
 
 	for {
 		select {
 		case <-stop:
-			log.Printf("writer STOP n:%d\n", n)
+			log.Printf("writer STOP\n")
 			wdone <- true
 			close(wdone)
 			return
 		default:
-			line := simulateLogInput()
-
-			// REVU: all this needs to be extracted as an io.Writer. BEGIN
-			_, e := file.Write(line)
-			panics.OnError(e, "file.Write", file)
-
-			n += int64(len(line))
-			if n > maxsize {
-				file, e = rotator.Next()
-				panics.OnError(e, "rotate", "on-rotate")
-				n = 0
-			}
-			// REVU: all this needs to be extracted as an io.Writer. END
+			_, e := rotator.Write(simulateLogInput())
+			panics.OnError(e, "rotator.Write")
 
 			time.Sleep(delay_msec)
 		}
@@ -104,89 +86,118 @@ func newProcess() (stop <-chan interface{}, wdone chan interface{}) {
 	return make(chan interface{}, 1), make(chan interface{}, 1)
 }
 
-// ----------------------------------------------------------------------
-// File Rotator
-// ----------------------------------------------------------------------
-
-// REVU: all this needs to be extracted as an io.Writer. BEGIN
-
-type fileRotator struct {
-	basepath, filepath string
-	file               *os.File
-	sequence, limit    uint
-}
-
-type FileRotator interface {
-	Next() (*os.File, error)
-}
-
-func GetFileRotatorWithDefaults(file *os.File) (r FileRotator, err error) {
-	return GetFileRotator(file, ".", uint(0), uint(16))
-}
-
-func GetFileRotator(file *os.File, basepath string, initseq, maxseq uint) (r FileRotator, err error) {
-	defer panics.Recover(&err)
-
-	// assert a few facts
-	panics.OnTrue(file == nil, "GetFileRotator", "file is nil")
-	panics.OnTrue(basepath == "", "GetFileRotator", "basepath is nil")
-	panics.OnTrue(maxseq < initseq, "GetFileRotator", "maxseq < initseq")
-	_, e := os.Open(basepath) // check basepath exists
-	panics.OnError(e, "GetFileRotator")
-	info, e := file.Stat()
-	panics.OnError(e, "GetFileRotator")
-	filepath := path.Join(basepath, file.Name())
-	xinfo, e := os.Stat(filepath)
-	panics.OnError(e, "GetFileRotator")
-	os.SameFile(info, xinfo)
-
-	return &fileRotator{basepath, filepath, file, initseq, maxseq}, nil
-}
-func (r *fileRotator) Next() (newfile *os.File, err error) {
-
-	newfile, newseq, e := rotate(r.file, r.sequence, r.limit)
-	if err != nil {
-		return nil, fmt.Errorf("rotating file %s - cause: %s", r.filepath, e.Error())
-	}
-	r.file = newfile
-	r.sequence = newseq
-
-	return
-}
-
-func rotate(file *os.File, seq uint, seqmax uint) (newfile *os.File, newseq uint, err error) {
-	defer panics.Recover(&err)
-
-	oldname := file.Name()
-	if seq == seqmax-1 {
-		seq = 0
-	} else {
-		seq++
-	}
-	newname := fmt.Sprintf("%s.%d", oldname, seq)
-	log.Printf("writer ROTATING %s to %s\n", oldname, newname)
-
-	var e error
-	info, e := file.Stat()
-	panics.OnError(e, "file.Stat", file)
-	filemode := info.Mode()
-	e = file.Close()
-	panics.OnError(e, "file.Close", file)
-
-	e = os.Rename(oldname, newname)
-	panics.OnError(e, "os.Rename", oldname, newname)
-
-	newfile, e = os.OpenFile(oldname, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, filemode)
-	panics.OnError(e, "os.OpenFile(O_CREATE..)", oldname)
-
-	return newfile, seq, nil
-}
-// REVU: all this needs to be extracted as an io.Writer. END
-
-var sequence uint64
-
-func simulateLogInput() []byte {
-	line := fmt.Sprintf("%d %019d INFO simulated single line sequenced log entry\n", time.Now().UnixNano(), sequence)
-	sequence++
-	return []byte(line)
-}
+//// ----------------------------------------------------------------------
+//// Rotating File Writer
+//// ----------------------------------------------------------------------
+//
+//type rotatingFileWriter struct {
+//	basepath, filepath string
+//	file               *os.File
+//	sequence, limit    uint
+//	maxFileSize        int64
+//	offset             int64
+//}
+//
+//type RotatingFileWriter interface {
+//	io.Writer
+////	rotate() (*os.File, error)
+//}
+//
+//func GetFileRotatorWithDefaults(basepath, basename string) (r RotatingFileWriter, err error) {
+//	return NewRotatingFileWriter(basepath, basename, uint(16), int64(1<<24))
+//}
+//
+//func NewRotatingFileWriter(basepath, basename string, maxseq uint, maxFileSize int64) (rotator *rotatingFileWriter, err error) {
+//
+//	defer panics.Recover(&err)
+//
+//	fname := path.Join(basepath, basename)
+//	file, e := os.OpenFile(fname, os.O_WRONLY|os.O_CREATE, config.fileperm)
+//	if e != nil {
+//		file, e = os.OpenFile(fname, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, config.fileperm)
+//		panics.OnError(e, "os.OpenFile", "CREATE|TRUNC", fname)
+//	}
+//
+//	_, e = os.Open(basepath) // check basepath exists
+//	panics.OnError(e, "NewFileRotator")
+//
+//	info, e := file.Stat()
+//	panics.OnError(e, "NewFileRotator")
+//
+//	filepath := path.Join(basepath, file.Name())
+//	xinfo, e := os.Stat(filepath)
+//	panics.OnError(e, "NewFileRotator")
+//	os.SameFile(info, xinfo)
+//
+//
+//	offset, _ := file.Seek(0, os.SEEK_END)
+//	rotator = &rotatingFileWriter{basepath, filepath, file, 0, maxseq, maxFileSize, offset}
+//
+//	e = rotator.rotateOnLimit()
+//	panics.OnError(e, "NewFileRotator", "rotateOnLimit:")
+//	return rotator, nil
+//}
+//
+//func (r *rotatingFileWriter) Write(p []byte) (n int, err error) {
+//	defer panics.Recover(&err)
+//
+//	n, err = r.file.Write(p)
+//	if err != nil {
+//		return
+//	}
+//	r.offset += int64(n)
+//	r.rotateOnLimit()
+//	return
+//}
+//
+//func (r *rotatingFileWriter) rotateOnLimit() error {
+//	if r.offset > r.maxFileSize {
+//		fmt.Printf("DEBUG: rotatingFileWriter: rotateOnLimit: file:%s offset:%d limit:%d\n", r.file.Name(), r.offset, r.maxFileSize)
+//		file, e := r.rotate()
+//		if e != nil {
+//			return e
+//		}
+//		r.file = file
+//		r.offset = 0
+//	}
+//	return nil
+//}
+//
+//func (r *rotatingFileWriter) rotate() (newfile *os.File, err error) {
+//
+//	newfile, newseq, e := rotate(r.file, r.sequence, r.limit)
+//	if err != nil {
+//		return nil, fmt.Errorf("rotating file %s - cause: %s", r.filepath, e.Error())
+//	}
+//	r.file = newfile
+//	r.sequence = newseq
+//
+//	return
+//}
+//
+//func rotate(file *os.File, seq uint, seqmax uint) (newfile *os.File, newseq uint, err error) {
+//	defer panics.Recover(&err)
+//
+//	oldname := file.Name()
+//	if seq == seqmax-1 {
+//		seq = 0
+//	} else {
+//		seq++
+//	}
+//	newname := fmt.Sprintf("%s.%d", oldname, seq)
+//
+//	var e error
+//	info, e := file.Stat()
+//	panics.OnError(e, "file.Stat", file)
+//	filemode := info.Mode()
+//	e = file.Close()
+//	panics.OnError(e, "file.Close", file)
+//
+//	e = os.Rename(oldname, newname)
+//	panics.OnError(e, "os.Rename", oldname, newname)
+//
+//	newfile, e = os.OpenFile(oldname, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, filemode)
+//	panics.OnError(e, "os.OpenFile(O_CREATE..)", oldname)
+//
+//	return newfile, seq, nil
+//}
---
+++ lslib/rotating-logger.go
@@ -0,0 +1,179 @@
+package lslib
+
+import (
+	"fmt"
+	"io"
+	"lsf/panics"
+	"os"
+	"os/signal"
+	"path"
+)
+
+// ----------------------------------------------------------------------
+// API
+// ----------------------------------------------------------------------
+
+// An append log writer with auto-rotation
+// Exposes the io.Writer interface.
+type RotatingFileWriter interface {
+	io.Writer
+	// closes the Writer.
+	// If already closed, it is a nop.
+	// Calls to Write() after close() will panic.
+	Close()
+	CloseOnSignal(sig ...os.Signal)
+}
+
+// ----------------------------------------------------------------------
+// Support
+// ----------------------------------------------------------------------
+type rotatingFileWriter struct {
+	basepath, filepath string
+	file               *os.File
+	sequence, limit    uint
+	maxFileSize        int64
+	offset             int64
+	closed             bool
+}
+
+// Returns a new RotatingFileWiter with default settings:
+// max number of files: 16
+// max size: 16777216 bytes
+// See NewRotatingFileWriter for additional details
+func GetFileRotatorWithDefaults(basepath, basename string) (r RotatingFileWriter, err error) {
+	return NewRotatingFileWriter(basepath, basename, uint(16), int64(1<<24))
+}
+
+// Returns a new RotatingFileWiter.
+// Files are closed on rotation. Active file will close on os.Interrupt | os.Kill.
+// This implementation of RotatingFileWriter does not support concurrent client.
+func NewRotatingFileWriter(basepath, basename string, maxseq uint, maxFileSize int64) (rotator *rotatingFileWriter, err error) {
+
+	defer panics.Recover(&err)
+
+	fileperm := os.FileMode(0644)
+
+	fname := path.Join(basepath, basename)
+	file, e := os.OpenFile(fname, os.O_WRONLY|os.O_CREATE, fileperm)
+	if e != nil {
+		file, e = os.OpenFile(fname, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, fileperm)
+		panics.OnError(e, "os.OpenFile", "CREATE|TRUNC", fname)
+	}
+
+	_, e = os.Open(basepath) // check basepath exists
+	panics.OnError(e, "NewFileRotator")
+
+	info, e := file.Stat()
+	panics.OnError(e, "NewFileRotator")
+
+	filepath := path.Join(basepath, file.Name())
+	xinfo, e := os.Stat(filepath)
+	panics.OnError(e, "NewFileRotator")
+	os.SameFile(info, xinfo)
+
+	offset, _ := file.Seek(0, os.SEEK_END)
+	rotator = &rotatingFileWriter{basepath, filepath, file, 0, maxseq, maxFileSize, offset, false}
+
+	e = rotator.rotateOnLimit()
+	panics.OnError(e, "NewFileRotator", "rotateOnLimit:")
+
+	return rotator, nil
+}
+
+// ----------------------------------------------------------------------
+// interface: RotatingFileWriter
+// ----------------------------------------------------------------------
+func (r *rotatingFileWriter) Write(p []byte) (n int, err error) {
+	defer panics.Recover(&err)
+
+	if r.closed {
+		err = fmt.Errorf("rotatingFIleWriter: Write: illegal state: closed")
+		return
+	}
+	n, err = r.file.Write(p)
+	if err != nil {
+		return
+	}
+	r.offset += int64(n)
+	r.rotateOnLimit()
+	return
+}
+func (r *rotatingFileWriter) Close() {
+	if r.closed {
+		return
+	}
+	r.file.Close()
+	r.closed = true
+}
+
+func (r *rotatingFileWriter) CloseOnSignal(signals ...os.Signal) {
+	osSignal := make(chan os.Signal, 1)
+	signal.Notify(osSignal, signals...)
+	go func() {
+		select {
+		case <-osSignal:
+			r.Close()
+			cleanClose(osSignal)
+			return
+		}
+	}()
+}
+func cleanClose(ch chan os.Signal) {
+	defer func() { recover() }()
+	close(ch)
+}
+
+// ----------------------------------------------------------------------
+// inner ops
+// ----------------------------------------------------------------------
+func (r *rotatingFileWriter) rotateOnLimit() error {
+	if r.offset > r.maxFileSize {
+		fmt.Printf("DEBUG: rotatingFileWriter: rotateOnLimit: file:%s offset:%d limit:%d\n", r.file.Name(), r.offset, r.maxFileSize)
+		file, e := r.rotate()
+		if e != nil {
+			return e
+		}
+		r.file = file
+		r.offset = 0
+	}
+	return nil
+}
+
+func (r *rotatingFileWriter) rotate() (newfile *os.File, err error) {
+
+	newfile, newseq, e := rotate(r.file, r.sequence, r.limit)
+	if err != nil {
+		return nil, fmt.Errorf("rotating file %s - cause: %s", r.filepath, e.Error())
+	}
+	r.file = newfile
+	r.sequence = newseq
+
+	return
+}
+
+func rotate(file *os.File, seq uint, seqmax uint) (newfile *os.File, newseq uint, err error) {
+	defer panics.Recover(&err)
+
+	oldname := file.Name()
+	if seq == seqmax-1 {
+		seq = 0
+	} else {
+		seq++
+	}
+	newname := fmt.Sprintf("%s.%d", oldname, seq)
+
+	var e error
+	info, e := file.Stat()
+	panics.OnError(e, "file.Stat", file)
+	filemode := info.Mode()
+	e = file.Close()
+	panics.OnError(e, "file.Close", file)
+
+	e = os.Rename(oldname, newname)
+	panics.OnError(e, "os.Rename", oldname, newname)
+
+	newfile, e = os.OpenFile(oldname, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, filemode)
+	panics.OnError(e, "os.OpenFile(O_CREATE..)", oldname)
+
+	return newfile, seq, nil
+}
