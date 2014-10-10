80a0d04c524880aa464d6f8665363e3370b2c613
Piotr Polatowski <piotr.polatowski@findwise.com>
2014-10-10 10:25:47 +0200

    Added property file managment. Properties in the property file defined in following pattern: prop=value
    properties in configuration file:

--- config.go
+++ config.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"os"
 	"time"
+	"strings"
+	"bufio"
 )
 
 const configFileSizeLimit = 10 << 20
@@ -37,7 +39,36 @@ type FileConfig struct {
 	deadtime time.Duration
 }
 
-func LoadConfig(path string) (config Config, err error) {
+func LoadProperties(properties_path string, config_in []byte) (config_out []byte, err error) {
+	if(properties_path == "") {
+		config_out = config_in
+		return
+	}
+	
+	config_str := string(config_in[:len(config_in)])	
+	
+	inputFile, err := os.Open(properties_path)
+	if err != nil {
+		emit("Error opening properties file: %s\n", err)
+	}	
+	defer inputFile.Close()
+	scanner := bufio.NewScanner(inputFile)
+
+	for scanner.Scan() {
+		prop := strings.Split(scanner.Text(), "=")
+		emit("Trying to replace property: " + prop[0] + " with value: " + prop[1])
+		config_str = strings.Replace(config_str, "${"+prop[0]+"}", prop[1], -1)
+	}
+	
+	if err := scanner.Err(); err != nil {
+		emit("Error occurred when reading the properties file: %s\n", scanner.Err())
+	}	
+
+	config_out = []byte(config_str)
+	return
+}
+
+func LoadConfig(path string, properties_path string) (config Config, err error) {
 	config_file, err := os.Open(path)
 	if err != nil {
 		emit("Failed to open config file '%s': %s\n", path, err)
@@ -52,8 +83,10 @@ func LoadConfig(path string) (config Config, err error) {
 
 	buffer := make([]byte, fi.Size())
 	_, err = config_file.Read(buffer)
-	emit("%s\n", buffer)
-
+	
+	// replacing properties
+	buffer, err = LoadProperties(properties_path, buffer)
+		
 	err = json.Unmarshal(buffer, &config)
 	if err != nil {
 		emit("Failed unmarshalling json: %s\n", err)
