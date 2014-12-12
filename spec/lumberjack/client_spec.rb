3c40a46899d99fbf56bf53cc53eaf6a553bcb529
Pier-Hugues Pellerin <phpellerin@gmail.com>
2014-12-12 09:04:31 -0800

    Adding a multibytes test with japanese characters

--- spec/lumberjack/client_spec.rb
+++ spec/lumberjack/client_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'spec_helper'
 require 'lumberjack/client'
 require 'lumberjack/server'
@@ -13,6 +14,17 @@ describe Lumberjack::Encoder do
     parser.feed(Lumberjack::Encoder.to_frame(content, 0)) do |code, sequence, data|
       expect(data["message"].force_encoding('UTF-8')).to eq(content["message"])
       expect(data["other"].force_encoding('UTF-8')).to eq(content["other"])
+    end
+  end
+
+  it 'should creates frames without dropping multibytes characters' do
+    content = {
+      "message" => "国際ホッケー連盟" # International Hockey Federation
+    }
+
+    parser = Lumberjack::Parser.new
+    parser.feed(Lumberjack::Encoder.to_frame(content, 0)) do |code, sequence, data|
+      expect(data["message"].force_encoding('UTF-8')).to eq(content["message"])
     end
   end
 end
