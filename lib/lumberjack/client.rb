5fd7dd8febb928c21f0100b0e9d2c9af7ef9fd6b
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-02-06 17:00:10 +0100

    Made sure the sequence number is of size unsigned 32 bits int, changes on the say some methods to make the code a bit more organized.
    On the way created a couple of test for the windows_size acks and the
    sequence numbers.

--- lib/lumberjack/client.rb
+++ lib/lumberjack/client.rb
@@ -4,8 +4,8 @@ require "openssl"
 require "zlib"
 
 module Lumberjack
-  
-  SEQUENCE_MAX = (2**(0.size * 8 -2) -1)
+
+  SEQUENCE_MAX = (2**32-1).freeze
 
   class Client
     def initialize(opts={})
@@ -73,22 +73,21 @@ module Lumberjack
       @host = @opts[:address]
       @window_size = @opts[:window_size]
 
-      tcp_socket = TCPSocket.new(@opts[:address], @opts[:port])
-      openssl_cert = OpenSSL::X509::Certificate.new(File.read(@opts[:ssl_certificate]))
+      connection_start(opts)
+    end
+
+    private
+    def connection_start(opts)
+      tcp_socket = TCPSocket.new(opts[:address], opts[:port])
       @socket = OpenSSL::SSL::SSLSocket.new(tcp_socket)
       @socket.connect
-
-      #if @socket.peer_cert.to_s != openssl_cert.to_s
-      #  raise "Client and server certificates do not match."
-      #end
-
       @socket.syswrite(["1", "W", @window_size].pack("AAN"))
     end
 
     private 
     def inc
-      @sequence = 0 if @sequence+1 > Lumberjack::SEQUENCE_MAX
-      @sequence += 1
+      @sequence = 0 if @sequence + 1 > Lumberjack::SEQUENCE_MAX
+      @sequence = @sequence + 1
     end
 
     private
@@ -100,17 +99,32 @@ module Lumberjack
     public
     def write_hash(hash)
       frame = to_frame(hash, inc)
-      ack if (@sequence - (@last_ack + 1)) >= @window_size
+      ack if unacked_sequence_size >= @window_size
       write frame
     end
 
     private
     def ack
-      version = @socket.read(1)
-      type = @socket.read(1)
+      _, type = read_version_and_type
       raise "Whoa we shouldn't get this frame: #{type}" if type != "A"
-      @last_ack = @socket.read(4).unpack("N").first
-      ack if (@sequence - (@last_ack + 1)) >= @window_size
+      @last_ack = read_last_ack
+      ack if unacked_sequence_size >= @window_size
+    end
+
+    private
+    def unacked_sequence_size
+      sequence - (@last_ack + 1)
+    end
+
+    private
+    def read_version_and_type
+      version = @socket.read(1)
+      type    = @socket.read(1)
+      [version, type]
+    end
+    private
+    def read_last_ack
+      @socket.read(4).unpack("N").first
     end
 
     private
