a73e1cb7e43c6de97050912b5bb35910c0f8d0da
Jordan Sissel <jls@semicomplete.com>
2015-02-10 14:08:40 -0800

    Make rpm and deb packaging happen again
    Includes package acceptance tests to make sure packages work.
    
    Unrelated, but it felt good to do:
    This purges all the old 3rd party vendor fetching stuff that was
    used in the older C implementation of logstash-forwarder, and
    this also removes some experimental Go stuff I had been working on.
    
    Fixes #368

--- Gemfile
+++ Gemfile
@@ -3,5 +3,7 @@ source 'https://rubygems.org'
 gem "rspec"
 gem "insist"
 gem "stud"
+gem "fpm"
+gem "pleaserun"
 
 gem "jruby-openssl", :platform => :jruby
---
+++ spec/acceptance/packaging_spec.rb
@@ -0,0 +1,36 @@
+
+
+
+describe "packaging" do
+  let(:version) { `./logstash-forwarder -version`.chomp }
+  before do
+    if !File.exist?("logstash-forwarder")
+      system("make logstash-forwarder")
+    end
+  end
+
+  describe "make rpm" do
+    let(:architecture) { RbConfig::CONFIG["host_cpu"] }
+    it "should build an rpm" do
+      system("make rpm")
+      expect($?).to be_success
+      expect(File).to be_exist("logstash-forwarder-#{version}-1.#{architecture}.rpm")
+    end
+  end
+
+  describe "make deb" do
+    let(:architecture) do
+      a = RbConfig::CONFIG["host_cpu"]
+      case a
+        when "x86_64"; "amd64" # why? Because computers.
+        else a
+      end
+    end
+    it "should build a deb" do
+      system("make deb")
+      expect($?).to be_success
+      expect(File).to be_exist("logstash-forwarder_#{version}_#{architecture}.deb")
+    end
+  end
+end
+
