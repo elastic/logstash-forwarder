94cdd0a372332fa55634923fb39e1042b421d641
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-02-11 12:05:24 +0100

    make the server codec agnostic, but making the accept method to handle a block where the user can create their custom connection object
    reformat spacing style
    
    Fixes #364

--- lib/lumberjack/server.rb
+++ lib/lumberjack/server.rb
@@ -6,7 +6,6 @@ require "zlib"
 module Lumberjack
   class Server
     attr_reader :port
-    attr_reader :codec
 
     # Create a new Lumberjack server.
     #
@@ -23,7 +22,7 @@ module Lumberjack
         :address => "0.0.0.0",
         :ssl_certificate => nil,
         :ssl_key => nil,
-        :ssl_key_passphrase => nil,
+        :ssl_key_passphrase => nil
       }.merge(options)
 
       [:ssl_certificate, :ssl_key].each do |k|
@@ -33,7 +32,7 @@ module Lumberjack
       end
 
       @tcp_server = TCPServer.new(@options[:address], @options[:port])
-      
+
       # Query the port in case the port number is '0'
       # TCPServer#addr == [ address_family, port, address, address ]
       @port = @tcp_server.addr[1]
@@ -42,26 +41,29 @@ module Lumberjack
       @ssl.key = OpenSSL::PKey::RSA.new(File.read(@options[:ssl_key]),
                                         @options[:ssl_key_passphrase])
       @ssl_server = OpenSSL::SSL::SSLServer.new(@tcp_server, @ssl)
-
     end # def initialize
 
-    def run(codec, &block)
+    def run(&block)
       while true
-        connection = accept(codec)
+        connection = accept
         Thread.new(connection) do |connection|
           connection.run(&block)
         end
       end
     end # def run
 
-    def accept(codec)
+    def accept(&block)
       begin
         fd = @ssl_server.accept
       rescue EOFError, OpenSSL::SSL::SSLError, IOError
         # ssl handshake or other accept-related failure.
         # TODO(sissel): Make it possible to log this.
       end
-      Connection.new(fd, codec)
+      if block_given?
+        block.call(fd)
+      else
+        Connection.new(fd)
+      end
     end
   end # class Server
 
@@ -95,16 +97,16 @@ module Lumberjack
       while have?(@need)
         send(@state, &block) 
         #case @state
-          #when :header; header(&block)
-          #when :window_size; window_size(&block)
-          #when :data_lead; data_lead(&block)
-          #when :data_field_key_len; data_field_key_len(&block)
-          #when :data_field_key; data_field_key(&block)
-          #when :data_field_value_len; data_field_value_len(&block)
-          #when :data_field_value; data_field_value(&block)
-          #when :data_field_value; data_field_value(&block)
-          #when :compressed_lead; compressed_lead(&block)
-          #when :compressed_payload; compressed_payload(&block)
+        #when :header; header(&block)
+        #when :window_size; window_size(&block)
+        #when :data_lead; data_lead(&block)
+        #when :data_field_key_len; data_field_key_len(&block)
+        #when :data_field_key; data_field_key(&block)
+        #when :data_field_value_len; data_field_value_len(&block)
+        #when :data_field_value; data_field_value(&block)
+        #when :data_field_value; data_field_value(&block)
+        #when :compressed_lead; compressed_lead(&block)
+        #when :compressed_payload; compressed_payload(&block)
         #end # case @state
       end
       return nil
@@ -139,10 +141,10 @@ module Lumberjack
       version, frame_type = get.bytes.to_a[0..1]
 
       case frame_type
-        when FRAME_WINDOW; transition(:window_size, 4)
-        when FRAME_DATA; transition(:data_lead, 8)
-        when FRAME_COMPRESSED; transition(:compressed_lead, 4)
-        else; raise "Unknown frame type: #{frame_type}"
+      when FRAME_WINDOW; transition(:window_size, 4)
+      when FRAME_DATA; transition(:data_lead, 8)
+      when FRAME_COMPRESSED; transition(:compressed_lead, 4)
+      else; raise "Unknown frame type: #{frame_type}"
       end
     end
 
@@ -192,7 +194,7 @@ module Lumberjack
       length = get.unpack("N").first
       transition(:compressed_payload, length)
     end
-    
+
     def compressed_payload(&block)
       original = Zlib::Inflate.inflate(get)
       transition(:header, 2)
@@ -203,11 +205,10 @@ module Lumberjack
   end # class Parser
 
   class Connection
-    def initialize(fd, codec)
+    def initialize(fd)
       super()
       @parser = Parser.new
       @fd = fd
-      @codec = codec
       @last_ack = 0
 
       # a safe default until we are told by the client what window size to use
@@ -223,8 +224,8 @@ module Lumberjack
         # X: too many events after errors.
         @parser.feed(@fd.sysread(16384)) do |event, *args|
           case event
-            when :window_size; window_size(*args, &block)
-            when :data; data(*args, &block)
+          when :window_size; window_size(*args, &block)
+          when :data; data(*args, &block)
           end
           #send(event, *args)
         end # feed
@@ -245,7 +246,7 @@ module Lumberjack
     end
 
     def data(sequence, map, &block)
-      block.call(map, @codec)
+      block.call(map)
       if (sequence - @last_ack) >= @window_size
         @fd.syswrite(["1A", sequence].pack("A*N"))
         @last_ack = sequence
