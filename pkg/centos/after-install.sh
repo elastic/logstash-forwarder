9997566d5fdf2c19fe38e22954851ae1db7e2570
Jordan Sissel <jls@semicomplete.com>
2015-03-03 13:22:21 -0800

    Tell the human who installs the package where to find logs
    I would do this in the `/etc/init.d/logstash-forwarder start` output,
    but I'm pretty sure systems like puppet/chef/etc rely on specific
    confines of the output of init scripts, so there's probably nothing we
    can output there. I'd love to know if this isn't true.

--- pkg/centos/after-install.sh
+++ pkg/centos/after-install.sh
@@ -3,3 +3,5 @@
 chown -R logstash-forwarder:logstash-forwarder /opt/logstash-forwarder
 chown logstash-forwarder /var/log/logstash-forwarder
 chown logstash-forwarder:logstash-forwarder /var/lib/logstash-forwarder
+
+echo "Logs for logstash-forwarder will be in /var/log/logstash-forwarder/"
--- pkg/ubuntu/after-install.sh
+++ pkg/ubuntu/after-install.sh
@@ -4,3 +4,5 @@ chown -R logstash-forwarder:logstash-forwarder /opt/logstash-forwarder
 chown logstash-forwarder /var/log/logstash-forwarder
 chown logstash-forwarder:logstash-forwarder /var/lib/logstash-forwarder
 update-rc.d logstash-forwarder defaults
+
+echo "Logs for logstash-forwarder will be in /var/log/logstash-forwarder/"
