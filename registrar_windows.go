c2819a3b8bd756fb9a2c51b78a10e7bbc3c10d03
Aldo Funes <aldo@funes.mx>
2015-07-14 10:25:57 -0500

    Add Windows Support for the .logstash-forwarder file from overwrite to rewrite

--- registrar_windows.go
+++ registrar_windows.go
@@ -8,6 +8,7 @@ func onRegistryWrite(path, tempfile string) error {
 	old := path + ".old"
 	var e error
 	
+	os.Remove(old);
 	if e = os.Rename(path, old); e != nil {
 		emit("registry rotate: rename of %s to %s - %s\n", path, old, e)
 		return e
